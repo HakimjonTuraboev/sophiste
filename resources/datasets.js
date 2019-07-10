7e9c95ea220a3e3d7dbbf383c075e79ae6f283ad
Hakim <hturab2s@gmail.com>
2019-07-10 23:40:40 +0200

    added project files

---
+++ resources/datasets.js
@@ -0,0 +1,302 @@
+/**
+ * @overview datasets for ccm component sophist
+ * @version 0.0.1
+ * @author mkaul2m Manfred.Kaul@h-brs.de
+ * @copyright The MIT License (MIT) mkaul2m on 13.06.2019.
+ */
+
+ccm.files[ 'datasets.js' ] = {
+   //Demo oder Funktionsmaster ohne Bedingung
+  "demo": {
+    "key": "demo",
+    "rows": [
+      {
+        system: "Das System",
+        modal: "MUSS",
+        func: "die Möglichkeit bieten",
+        object: "die Bezahlung",
+        process: "zu berechnen"
+      },
+      {
+        system: "Das Kassensystem",
+        modal: "SOLLTE",
+        func: "fähig sein",
+        object: "die Daten",
+        process: "zu speichern"
+      }
+    ]
+
+  },
+  //Funktionsmaster mit Bedingung
+  "Funktionsmaster_mB": {
+    "key": "Funktionsmaster_mB",
+    "rows": [
+      {
+        bedingung: "Solange der Scanner an ist",
+        system: "das System",
+        modal: "MUSS",
+        func: "die Möglichkeit bieten",
+        object: "die Bezahlung",
+        process: "zu berechnen"
+      },
+      {
+        bedingung: "Sobald Fehler auftritt",
+        system: "das Kassensystem",
+        modal: "SOLLTE",
+        func: "fähig sein",
+        object: "die Daten",
+        process: "zu speichern"
+      }
+    ]
+  },
+  // Detaillierter Funktionsmaster ohne Bedingung
+  "D_Funktionsmaster_oB": {
+    "key": "D_Funktionsmaster_oB",
+    "rows": [
+      {
+        system: "Das System",
+        modal: "MUSS",
+        func: "fähig sein",
+        präzisiere1:"die selektierten",
+        object: "Bezahlungsdaten",
+        präzisiere2:"eines regstrierten Kunden",
+        konkretisiere:"auf dem CD",
+        process: "zu speichern"
+      },
+
+    ]
+
+  },
+  // Detaillierter Funktionsmaster mit Bedingung
+  "D_Funktionsmaster_mB": {
+    "key": "D_Funktionsmaster_mB",
+    "rows": [
+      {
+        bedingung:"Sobald Systemspeicher voll ist,",
+        system: "Das System",
+        modal: "MUSS",
+        func: "fähig sein",
+        präzisiere1:"die selektierten",
+        object: "Bezahlungsdaten",
+        präzisiere2:"eines regstrierten Kunden",
+        konkretisiere:"auf dem CD",
+        process: "zu speichern"
+      },
+    ]
+  },
+  // Eigenschaftsmaster
+  "Eigenschaftsmaster": {
+    "key": "Eigenschaftsmaster",
+    "rows": [
+      {
+        eigenschaft:"Die Länge",
+        betrachtendes:"des Eingabefensters",
+        modal: "MUSS",
+        vergleich:"gleich",
+        wert:"lang",
+        sein: "sein",
+      },
+    ]
+  },
+  // Umgebungsmaster
+  "Umgebungsmaster":{
+    "key": "Umgebungsmaster",
+    "rows": [
+      {
+        komponente: "Lesegerät des Systems",
+        modal: "MUSS",
+        gestaltet: "so gestaltet sein, dass",
+        betrachtendes:"Bedingung + das System",
+        eigenschaft:"bei der Weite",
+        vergleich:"von 4 cm bis 100 cm",
+        betrieben:"betrieben werden kann",
+      },
+    ]
+  },
+
+  // Prozessmaster
+  "Prozessmaster": {
+    "key": "Prozessmaster",
+    "rows": [
+      {
+        system: "Der Auftraggeber",
+        modal: "MUSS",
+        präzisiere1:"die ausgelisteten",
+        object: "Mitarbeiter",
+        präzisiere2:"des Unternehmens",
+        konkretisiere:"jährlich",
+        process: "schulen"
+      },
+    ]
+  },
+
+  // Bedingungsmaster für logische Aussage
+  "Bedingungsmaster_logik": {
+    "key": "Bedingungsmaster_logik",
+    "rows": [
+      {
+        konjunkt2:"Falls",
+        präzisiere1:"die Karte gesperrt ist,",
+        präzisiere2: "Anforderungshauptsatz"
+      },
+    ]
+  },
+  // Bedingungsmaster für Ereignis
+  "Bedingungsmaster_ereignis": {
+    "key": "Bedingungsmaster_ereignis",
+    "rows": [
+      {
+        konjunkt1:"Sobald",
+        präzisiere1:"der Kunde bezahlt hat,",
+        präzisiere2: "Anforderungshauptsatz"
+      },
+    ]
+  },
+  // Bedingungsmaster für Zeitraum
+  "Bedingungsmaster_zeitraum": {
+    "key": "Bedingungsmaster_zeitraum",
+    "rows": [
+      {
+        konjunkt3:"Solange",
+        präzisiere1:"das System angeschaltet ist,",
+        präzisiere2: "Anforderungshauptsatz"
+      },
+    ],
+  },
+  // Functionalmaster without condition
+  "FunctionalM": {
+    "key": "FunctionalM",
+    "rows": [
+      {
+        system:"The labrary system",
+        modal_e:"SHALL",
+        func_e:"PROVIDE (labrarian WITH) ABILITY TO",
+        process: "print",
+        object:"the pdf-file",
+      },
+    ],
+  },
+
+  // Functionalmaster with condition
+  "FunctionalM_wC": {
+    "key": "FunctionalM_wC",
+    "rows": [
+      {
+        bedingung:"If the user is logged in,",
+        system:"the labrary system",
+        modal_e:"SHALL",
+        func_e:"PROVIDE (labrarian WITH) ABILITY TO",
+        process: "print",
+        object:"the pdf-file",
+      },
+    ],
+  },
+  // Detailed functionalmaster
+  "D_FunctionalM": {
+    "key": "D_FunctionalM",
+    "rows": [
+      {
+        system:"the labrary system",
+        modal_e:"SHALL",
+        func_e:"PROVIDE (labrarian WITH) ABILITY TO",
+        process: "print",
+        präzisiere1:" selected colored photos ",
+        konkretisiere:"at   the net-work printer",
+
+      },
+    ],
+  },
+  //Detailed functionalmaster with condition
+  "D_FunctionalMwC": {
+    "key": "D_FunctionalMwC",
+    "rows": [
+      {
+        bedingung:"If the user is logged in,",
+        system:"the labrary system",
+        modal_e:"SHALL",
+        func_e:"PROVIDE (labrarian WITH) ABILITY TO",
+        process: "print",
+        präzisiere1:"selected colored photos ",
+        konkretisiere:"at the net-work printer",
+      },
+    ],
+  },
+  //Properity Master
+  "D_FunctionalMwC": {
+    "key": "D_FunctionalMwC",
+    "rows": [
+      {
+        bedingung:"If the user is logged in,",
+        system:"the labrary system",
+        modal_e:"SHALL",
+        func_e:"PROVIDE (labrarian WITH) ABILITY TO",
+        process: "print",
+        präzisiere1:"selected colored photos ",
+        konkretisiere:"at the net-work printer",
+      },
+    ],
+  },
+  //Environment Master
+  "D_FunctionalMwC": {
+    "key": "D_FunctionalMwC",
+    "rows": [
+      {
+        bedingung:"If the user is logged in,",
+        system:"the labrary system",
+        modal_e:"SHALL",
+        func_e:"PROVIDE (labrarian WITH) ABILITY TO",
+        process: "print",
+        präzisiere1:"selected colored photos ",
+        konkretisiere:"at the net-work printer",
+      },
+    ],
+  },
+  //Process Master
+  "D_FunctionalMwC": {
+    "key": "D_FunctionalMwC",
+    "rows": [
+      {
+        bedingung:"If the user is logged in,",
+        system:"the labrary system",
+        modal_e:"SHALL",
+        func_e:"PROVIDE (labrarian WITH) ABILITY TO",
+        process: "print",
+        präzisiere1:"selected colored photos ",
+        konkretisiere:"at the net-work printer",
+      },
+    ],
+  },
+  //Condition logical expression
+  "condition_logical": {
+    "key": "condition_logical",
+    "rows": [
+      {
+        konjunkt2_e:"IF",
+        präzisiere1:"the card is blocked,",
+        präzisiere2: "requirements-main clause"
+      },
+    ]
+  },
+  // Condition event
+  "condition_event": {
+    "key": "condition_event",
+    "rows": [
+      {
+        konjunkt1_e:"AS SOON AS",
+        präzisiere1:"the customer has paid,",
+        präzisiere2: "requirements-main clause"
+      },
+    ]
+  },
+  // Condition time
+  "condition_time": {
+    "key": "condition_time",
+    "rows": [
+      {
+        konjunkt3_e:"AS LONG AS",
+        präzisiere1:"the system is switched on,",
+        präzisiere2: "requirements-main clause"
+      },
+    ],
+  },
+};
